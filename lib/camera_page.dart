7f5b4074f529fb12f53929d5517c106b8d04862d
Rumen <rumenmitov@protonmail.com>
2024-01-10 11:24:49 +0100

    feat: taking 5 images and sending them to server

--- lib/camera_page.dart
+++ lib/camera_page.dart
@@ -6,8 +6,7 @@ import 'package:camera/camera.dart';
 import 'package:http/http.dart' as http;
 
 Future<CameraDescription> initCamera() async {
-  // Ensure that plugin services are initialized so that `availableCameras()`
-  // can be called before `runApp()`
+  // Ensure that plugin services are initialized so that `availableCameras()` can be called
   WidgetsFlutterBinding.ensureInitialized();
 
   // Obtain a list of the available cameras on the device.
@@ -15,7 +14,6 @@ Future<CameraDescription> initCamera() async {
 
   // Get a specific camera from the list of available cameras and return it
   return cameras.first;
-
 }
 
 class BlindSightGuidance extends StatefulWidget {
@@ -81,28 +79,30 @@ class TakePictureState extends State<BlindSightGuidance> {
                     // NOTE: we need to find out how long it takes for AI to analyze an image
                     final serverDelay = 500;
 
-                    // TODO: create server
-                    final url = "https://127.0.0.1:3000";
+                    // TODO: save serverDelay and serverUrl in env variables
+                    final url = "http://10.0.2.2:3000/image";
 
                     var id = 0;
 
-                    while (true) {
+                    // TODO: change 5 to true when implementing with AI for constant inflow of images
+                    while (id < 5) {
                         final image = await _controller.takePicture();
                         sleep(Duration(milliseconds: serverDelay));
 
                         final image_bytes = await image.readAsBytes();
 
-                        http.post(Uri.parse(url), body: {
-                            "image": base64Encode(image_bytes),
-                            "name": "guidanceIMG_$id",
-                        }).then((res) {
-                            print(res.statusCode);
-                            id++;
-                        }).catchError((err) {
-                            print(err);
-                        });
+                        final request = http.MultipartRequest("POST", Uri.parse(url));
+                        request.fields["name"] = "guidanceIMG_$id";
+                        request.fields["image"] = base64Encode(image_bytes);
+
+                        request.send();
+
+                        id++;
                     }
+
+
                 },
+                child: Icon(Icons.camera_alt),
             ),
         );
     }
